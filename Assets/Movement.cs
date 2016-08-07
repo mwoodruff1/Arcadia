e9074a8c896f0a9ff1871ca0a713f861384a4fd8
Sigma Lungbarrow <backstop920@gmail.com>
2016-08-07 15:10:37 -0500

    Initial Commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Movement : MonoBehaviour{
+	
+	//Movement Variables
+	public float ShipTopSpeed = -10.0f;
+    public float ShipTopReverseSpeed = 10.0f;
+    public float ShipSpeed = 0.0f;
+    public float ShipSpeedIncrease = 0.1f;
+    public float ShipSpeedDecrease = -0.1f;
+    public float ShipTurnRate = 0.1f;
+    public float turnSpeed = 50f;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 forward;
+    
+    void Update(){
+        //All The Controller Initialization and Movement Crap
+        
+	CharacterController controller = GetComponent<CharacterController>();
+	moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	moveDirection = transform.TransformDirection(moveDirection);
+        controller.Move(this.gameObject.transform.forward * ShipSpeed);
+    
+        
+
+	if (Input.GetKey("s"))
+	{
+            if (ShipSpeed > ShipTopSpeed)
+            {
+                ShipSpeed += ShipSpeedIncrease;
+
+                if(ShipSpeed < ShipTopSpeed)
+                {
+                    ShipSpeed = ShipTopSpeed;
+                }}}
+
+        if (Input.GetKey("w"))
+        {
+            if (ShipSpeed < ShipTopReverseSpeed)
+            {
+                ShipSpeed += ShipSpeedDecrease;
+
+                if (ShipSpeed > ShipTopReverseSpeed)
+                {
+                    ShipSpeed = ShipTopReverseSpeed;
+                }            }       }
+        //Yaw 
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        //Pitch
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
+        //Rotation
+        if (Input.GetKey("q"))
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey("e"))
+            transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Forward Backwards
+        if (Input.GetKey("w"))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey("s"))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        //Yaw 
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        //Pitch
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
+        //Rotation
+        if (Input.GetKey("q"))
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey("e"))
+            transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+    }
+}

c669ecdd1e603f97238bb7117865b536a4510387
Sigma Lungbarrow <backstop920@gmail.com>
2016-08-08 09:15:55 -0500

    Hyperdrive Script Toggle Fixed
    You can now toggle the hyperdrive script

--- Assets/Hyperdrive.cs
+++ Assets/Hyperdrive.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class Hyperdrive : MonoBehaviour
 {
     public float HyperdriveSpeed = 0.0f;
-    bool HyperDriveToggle = false;
+    public bool HyperDriveToggle = false;
     void Update()
     {
-        
-        
-        if (Input.GetKey(KeyCode.H))
-        {
-            HyperDriveToggle = !HyperDriveToggle;
-        }
-        if (HyperDriveToggle)
+
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {HyperDriveToggle = !HyperDriveToggle;}
+        if (HyperDriveToggle == true)
             transform.Translate(Vector3.forward * HyperdriveSpeed * Time.deltaTime);
+        else
+            HyperDriveToggle = false;
     }
+    
+   
 }
 
 //transform.Translate(Vector3.forward* HyperdriveSpeed * Time.deltaTime);

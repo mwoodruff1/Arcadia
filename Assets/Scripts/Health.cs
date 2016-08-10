bf6d1b4fc5a753eafca5958ab7907baad2d1d484
Sigma Lungbarrow <backstop920@gmail.com>
2016-08-09 21:17:04 -0500

    Skeleton Coded Health
    Skeleton coded the health script.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    public float PlayerHealth = 100.0f;
+
+    // void Update just calls the health script which is farther down.
+    void Update()
+    {
+        HealthOfPlayer();    
+    }
+
+    //Below is the Health of Player script
+    void HealthOfPlayer()
+    {
+        if (PlayerHealth <= 0)
+        {
+            Debug.Log("Player Health is 0");
+        }
+        if (PlayerHealth > 100)
+            Debug.LogWarning("Player Health is greater than 100. This is a error.");
+
+        
+    }
+    //The below is not implemented
+    public void TakeDamage(float amount)
+    {
+        // Decrement the player's health by amount.
+        PlayerHealth -= amount;
+    }
+}

51972b177684f7f07de24bf2efada5833a4d2f1c
Sigma Lungbarrow <backstop920@gmail.com>
2016-08-10 21:35:25 -0500

    Added Power Gen Script
    Power Generation now works. Now other things can be implemented

--- Assets/Scripts/PowerBaseScript.cs
+++ Assets/Scripts/PowerBaseScript.cs
@@ -12,17 +12,24 @@ float PowerSystemOutput = 0.0f;
 //These are the formulas for the actual system. One is the Default Power Output and one is the Decay Formula.
 
 void Update() {
-            PowerDecayFormula = PowerSystemLevel / PowerSystemCap;
-            PowerSystemOutput = PowerDecayFormula - PowerSystemLevel;
+        //The Below is currently not active.
+          //  PowerDecayFormula = PowerSystemLevel / PowerSystemCap;
+          //  PowerSystemOutput = PowerDecayFormula - PowerSystemLevel;
+		
+		if (PowerSystemLevel > PowerSystemCap)
+			PowerSystemLevel = PowerSystemCap;
+        //The above will fix the constant increase of the power generation Camwarp 9:25 8/10/2016
+
+
         if (PowerSystemLevel == PowerSystemCap)
 	Debug.Log("Power System is at 100 Percent");
-        if(PowerSystemLevel == 1875.0f)
+        if(PowerSystemLevel <= 1875.0f)
 	Debug.Log("Power Level at 75 Percent");
-        if (PowerSystemLevel == 1250.0f)
+        if (PowerSystemLevel <= 1250.0f)
 	Debug.Log("Power System at 50 Percent");
-        if (PowerSystemLevel == 625.0f)
+        if (PowerSystemLevel <= 625.0f)
 	Debug.Log("Power System at 25 Percent");
-        if (PowerSystemLevel == 0.0f)
+        if (PowerSystemLevel < 0.0f)
             Debug.Log("Power System at 0 Percent");
 
 }
---
+++ Assets/Scripts/PowerGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerGenerator : MonoBehaviour
+{
+    public PowerBaseScript powerGenerationAccess;
+    private float PowerGenOutput;
+    private float PowerGenActualOutput = 0.0f;
+    private float PowerGenAddition = 25.0f;
+    void Update()
+    {
+        PowerGenOutput += PowerGenAddition + PowerGenActualOutput;
+        powerGenerationAccess.PowerSystemLevel = PowerGenOutput;
+
+        //Quick fix for power gen problem of constant increase
+        if (powerGenerationAccess.PowerSystemLevel > 2500)
+        {
+            powerGenerationAccess.PowerSystemLevel = 2500;
+        }
+    }
+}

71e2bdf2a0f2b61a982130ef899c2f2c6a584a15
Sigma Lungbarrow <backstop920@gmail.com>
2016-08-10 21:57:32 -0500

    Started adding power requirments to the Hyperdrive
    WIP

--- Assets/Scripts/Hyperdrive.cs
+++ Assets/Scripts/Hyperdrive.cs
@@ -5,19 +5,30 @@ public class Hyperdrive : MonoBehaviour
 {
     public float HyperdriveSpeed = 25.0f;
     private bool HyperDriveToggle = false;
+    public PowerBaseScript powerLevelAccess;
     void Update()
     {
-
-
-        if (Input.GetKeyDown(KeyCode.H))
-        {HyperDriveToggle = !HyperDriveToggle;}
-        if (HyperDriveToggle == true)
-            transform.Translate(Vector3.forward * HyperdriveSpeed * Time.deltaTime);
-        else
-            HyperDriveToggle = false;
+       /* if(powerLevelAccess.PowerSystemLevel < 500)
+        {
+            print("Power Level to low to engage Hyperdrive");
+        } */
+        HyperDrive();
+        
     }
-    
-   
+
+    void HyperDrive()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        { HyperDriveToggle = !HyperDriveToggle; }
+
+        if (HyperDriveToggle == true) { 
+        transform.Translate(Vector3.forward * HyperdriveSpeed * Time.deltaTime);
+        float PowerDecay = 500.0f;
+        powerLevelAccess.PowerSystemLevel -= PowerDecay;
+        }
+        else
+            HyperDriveToggle = false; 
+    }
 }
 
 //transform.Translate(Vector3.forward* HyperdriveSpeed * Time.deltaTime);
